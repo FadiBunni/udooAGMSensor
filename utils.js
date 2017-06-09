f9e2fa9522d5ad900f8e397b2567df96d3a7127a
FadiBunni <fadibunni_guitar@hotmail.com>
2017-06-09 21:15:32 +0000

    nyeste

---
+++ utils.js
@@ -0,0 +1,20 @@
+
+
+
+
+function _regExample(sensor){
+	if(sensor == 'a'){
+		console.log("Error: wrong register supplied to setSensConf(sensor,reg,hexVAL). Use one from the list instead:");
+		for 	
+	
+	}
+	
+}
+
+function _dataConvertion(Object,sensor,axisLsit,uM){
+
+
+
+}
+
+
