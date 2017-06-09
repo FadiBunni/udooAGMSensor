aaead8f0d25ebc7e13f49737ce9b2ef0af1ffb20
FadiBunni <fadibunni_guitar@hotmail.com>
2017-06-10 01:54:50 +0200

    adding methods inside utils

--- app.js
+++ app.js
@@ -1,63 +1,68 @@
-var fs = require('fs');
+const utils = require('./utils.js');
+const fs = require('fs');
 
-setInterval(readMotionSensors, 150);
+console.log(utils.hey());
 
-var zero = {
-	modulus: 0,
-	axis: [0,0,0]
-};
+//setInterval(readMotionSensors, 150);
 
-var acc = zero,
-	 gyro = zero,
-	 magn = zero;
-	 
-function readMotionSensors() {
-	fs.readFile('/sys/class/misc/FreescaleAccelerometer/data','utf8',function(err,data){
-		if(err){
-			acc = zero;
-			return;
-		}
-		var axis = data.split(",");
-		axis = [parseInt(axis[0]),parseInt(axis[1]),parseInt(axis[2])];
-		var modulus = Math.sqrt(axis[0]*axis[0]+axis[1]*axis[1]+axis[2]*axis[2]);
-		
-		acc = {
-			modulus: modulus,
-			axis: axis
-		};
-	});
-	
-	fs.readFile('/sys/class/misc/FreescaleGyroscope/data','utf8',function(err,data){
-		if(err){
-			acc = zero;
-			return;
-		}
-		var axis = data.split(",");
-		axis = [parseInt(axis[0]),parseInt(axis[1]),parseInt(axis[2])];
-		var modulus = Math.sqrt(axis[0]*axis[0]+axis[1]*axis[1]+axis[2]*axis[2]);
-		
-		gyro = { 
-			modulus: modulus,
-			axis: axis
-		};
-	});
-	
-	fs.readFile('/sys/class/misc/FreescaleMagnetometer/data','utf8',function(err,data){
-		if(err){
-			acc = zero;
-			return;
-		}
-		var axis = data.split(",");
-		axis = [parseInt(axis[0]),parseInt(axis[1]),parseInt(axis[2])];
-		var modulus = Math.sqrt(axis[0]*axis[0]+axis[1]*axis[1]+axis[2]*axis[2]);
-		
-		magn = {
-			modulus: modulus,
-			axis: axis
-		};
-	});
-	//console.log(acc);
-	//console.log(gyro);
-	console.log(magn);
-}
+// var zero = {
+// 	modulus: 0,
+// 	axis: [0,0,0]
+// };
+
+// var acc = zero,
+// 	 gyro = zero,
+// 	 magn = zero;
+
+// function readMotionSensors() {
+// 	fs.readFile('/sys/class/misc/FreescaleAccelerometer/data','utf8',function(err,data){
+// 		if(err){
+// 			acc = zero;
+// 			return;
+// 		}
+// 		var axis = data.split(",");
+// 		axis = [parseInt(axis[0]),parseInt(axis[1]),parseInt(axis[2])];
+// 		var modulus = Math.sqrt(axis[0]*axis[0]+axis[1]*axis[1]+axis[2]*axis[2]);
+
+// 		acc = {
+// 			modulus: modulus,
+// 			axis: axis
+// 		};
+// 	});
+
+// 	fs.readFile('/sys/class/misc/FreescaleGyroscope/data','utf8',function(err,data){
+// 		if(err){
+// 			acc = zero;
+// 			return;
+// 		}
+// 		var axis = data.split(",");
+// 		axis = [parseInt(axis[0]),parseInt(axis[1]),parseInt(axis[2])];
+// 		var modulus = Math.sqrt(axis[0]*axis[0]+axis[1]*axis[1]+axis[2]*axis[2]);
+
+// 		gyro = {
+// 			modulus: modulus,
+// 			axis: axis
+// 		};
+// 	});
+
+// 	fs.readFile('/sys/class/misc/FreescaleMagnetometer/data','utf8',function(err,data){
+// 		if(err){
+// 			acc = zero;
+// 			return;
+// 		}
+// 		var axis = data.split(",");
+// 		axis = [parseInt(axis[0]),parseInt(axis[1]),parseInt(axis[2])];
+// 		var modulus = Math.sqrt(axis[0]*axis[0]+axis[1]*axis[1]+axis[2]*axis[2]);
+
+// 		magn = {
+// 			modulus: modulus,
+// 			axis: axis
+// 		};
+// 	});
+// 	//console.log(acc);
+// 	//console.log(gyro);
+// 	console.log(magn);
+// }
+
+
 
--- utils.js
+++ utils.js
@@ -1,20 +1,46 @@
+var REGS = require('./regs.js');
+
+var utils = {
+	regExample: function(sensor) {
+		if(sensor == 'a'){
+			console.log("Error: wrong register supplied to setSensConf(sensor,reg,hexVAL). Use one from the list instead:");
+			for(var list in REGS.A_CREGS_LIST){
+				var l = REGS.A_CREGS_LIST[list];
+				console.log(l);
+			}
+			process.exit(1);
+		}
+
+		if(sensor == 'm'){
+			console.log("Error: wrong register supplied to setSensConf(sensor,reg,hexVAL). Use one from the list instead:");
+			for(var list in REGS.M_CREG_LIST){
+				var l = REGS.M_CREG_LIST[list];
+				console.log(l);
+			}
+			process.exit(1);
+		}
+
+		if(sensor == 'g'){
+			console.log("Error: wrong register supplied to setSensConf(sensor,reg,hexVAL). Use one from the list instead:");
+			for(var list in REGS.G_CREG_LIST){
+				var l = REGS.G_CREG_LIST[list];
+				console.log(l);
+			}
+			process.exit(1);
+		}
+	},
+
+	dataConvertion: function(object,sensor,axisList,uM){
+		var uM = (uM !== undefined) ? uM : false;
+		//console.log(uM);
+
+	},
+
+};
+
+module.exports = utils;
 
 
 
-
-function _regExample(sensor){
-	if(sensor == 'a'){
-		console.log("Error: wrong register supplied to setSensConf(sensor,reg,hexVAL). Use one from the list instead:");
-		for 	
-	
-	}
-	
-}
-
-function _dataConvertion(Object,sensor,axisLsit,uM){
-
-
-
-}
 
 
